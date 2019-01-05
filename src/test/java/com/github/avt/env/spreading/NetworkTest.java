063229842a0f2bb71148dc4591042d2e7e100b4a
sammers <titantins@gmail.com>
2019-01-05 03:33:27 +0300

    add not null requirement

--- src/test/java/com/github/avt/env/spreading/NetworkTest.java
+++ src/test/java/com/github/avt/env/spreading/NetworkTest.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 @RunWith(VertxUnitRunner.class)
@@ -26,6 +27,7 @@ public class NetworkTest {
       log.info("Google is reachable. We are able to run the test");
       Network network = new Network(Network.NetworkType.IPv4_INTERNET, Vertx.vertx());
       String address = network.getHostAddressInTheNetworkBlocking();
+      Objects.requireNonNull(address);
       testContext.assertTrue(validateAddr(address));
     } else {
       log.info("Google is not reachable. We are not able to run the test");
