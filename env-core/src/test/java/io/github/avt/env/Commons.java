1f828275b5f724d86f7f1e7c05b2874825328157
Sammers21 <titantins@gmail.com>
2019-05-19 22:16:16 +0300

    add raft jar file path

--- env-core/src/test/java/io/github/avt/env/Commons.java
+++ env-core/src/test/java/io/github/avt/env/Commons.java
@@ -5,4 +5,5 @@ import java.io.File;
 public class Commons {
   public final static String LOCALHOST = "127.0.0.1";
   public final static File TEST_FILE_WITH_P2P_VIRUS = new File("build/libs/env-core-p2p-test-fat.jar");
+  public final static File TEST_FILE_WITH_RAFT_VIRUS = new File("build/libs/env-core-raft-test-fat.jar");
 }
